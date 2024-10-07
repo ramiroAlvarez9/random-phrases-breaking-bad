8a1c702ece44c625abe24d32c57823542db0a2de
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2024-10-07 13:52:42 -0300

    feat: new styles in app

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,73 @@ function App(): JSX.Element {
     const response = await request.json();
     const phrase = v.parse(PhraseSchema, response[0])
     const { quote, author } = phrase;
+
     setPhrase({ quote, author });
   };
+  const containerStyle = {
+    textAlign: "center",
+    fontFamily: "Arial, sans-serif",
+    padding: "20px",
+    backgroundColor: "#333",
+    color: "#fff",
+    minHeight: "100vh",
+  };
 
+  const titleStyle = {
+    fontSize: "36px",
+    marginBottom: "20px",
+    color: "#0c8b33", // Breaking Bad green color
+    fontWeight: "bold",
+  };
+
+  const quoteStyle = {
+    fontSize: "24px",
+    fontStyle: "italic",
+    marginBottom: "20px",
+  };
+
+  const authorStyle = {
+    fontSize: "20px",
+    marginTop: "10px",
+    fontWeight: "lighter",
+    color: "#aaa",
+  };
+
+  const buttonStyle = {
+    padding: "10px 20px",
+    fontSize: "18px",
+    backgroundColor: "#0c8b33", // Breaking Bad green color
+    border: "none",
+    borderRadius: "5px",
+    color: "#fff",
+    cursor: "pointer",
+    transition: "background-color 0.3s ease",
+  };
+
+  const buttonHoverStyle = {
+    ...buttonStyle,
+    backgroundColor: "#065f23",
+  };
   return (
-    <>
-      <h1>Breaking Bad Random Phrases Generator</h1>
-      <p>{phrase.quote ? `"${phrase.quote}" - ${phrase.author}` : "No phrase yet"}</p>
-      <button onClick={() => getPhrase()}>Change Phrase</button>
-    </>
+    <div style={containerStyle}>
+      <h1 style={titleStyle}>Breaking Bad Random Phrases Generator</h1>
+      <p style={quoteStyle}>
+        {phrase.quote ? `"${phrase.quote}"` : "No phrase yet"}
+      </p>
+      <p style={authorStyle}>{phrase.author}</p>
+      <button
+        onMouseEnter={(e) => (e.target.style.backgroundColor = "#065f23")}
+        onMouseLeave={(e) => (e.target.style.backgroundColor = "#0c8b33")}
+        onClick={getPhrase}
+        style={buttonStyle}
+      >
+        Change Phrase
+      </button>
+    </div>
+
+
+
+
   );
 }
 
