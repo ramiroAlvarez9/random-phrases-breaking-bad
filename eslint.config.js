3f608921863bee34483970eaffb73155ed762bf0
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2024-10-06 23:54:30 -0300

    commit to push

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import antfu from "@antfu/eslint-config"
+
+export default antfu(
+  {
+    stylistic: {
+      quotes: "double",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "style/brace-style": ["error", "1tbs", { allowSingleLine: false }],
+      "style/space-before-function-paren": ["error", {
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "always",
+      }],
+      "ts/consistent-type-imports": [
+        "error",
+        {
+          disallowTypeAnnotations: true,
+          prefer: "type-imports",
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          "newlines-between": "always",
+          "alphabetize": {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "curly": ["error", "all"],
+      "no-nested-ternary": "error",
+      "unused-imports/no-unused-imports": "error",
+      "no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "all",
+          ignoreRestSiblings: false,
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-fallthrough": "error",
+      "no-console": "warn",
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["./*", "../*"],
+        },
+      ],
+    },
+  },
+)
---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import * as v from 'valibot';
+
+const PhraseSchema = v.object({
+  quote: v.string(),
+  author: v.string()
+})
+type Phrase = v.InferOutput<typeof PhraseSchema>
+function App(): JSX.Element {
+
+  const [phrase, setPhrase] = useState<Phrase>({ quote: "", author: "" });
+  const getPhrase = async () => {
+    const request = await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
+    const response = await request.json();
+    const phrase = v.parse(PhraseSchema, response[0])
+    const { quote, author } = phrase;
+    setPhrase({ quote, author });
+  };
+
+  return (
+    <>
+      <h1>Breaking Bad Random Phrases Generator</h1>
+      <p>{phrase.quote ? `"${phrase.quote}" - ${phrase.author}` : "No phrase yet"}</p>
+      <button onClick={() => getPhrase()}>Change Phrase</button>
+    </>
+  );
+}
+
+export default App;
